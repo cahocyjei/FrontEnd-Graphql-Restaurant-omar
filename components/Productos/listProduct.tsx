4114a93b996ab743eaf2f179ffb6ba98ae0579d5
Omar Cabarcas <cahocyjei@gmail.com>
2023-10-25 09:57:09 -0500

    Creando Rama omarCabarcas

--- components/Productos/listProduct.tsx
+++ components/Productos/listProduct.tsx
@@ -1,34 +1,27 @@
-import React from 'react'
+import React from "react";
 import Producto from "@models/producto";
 import styles from "@/styles/products.module.css";
-import Link from 'next/link';
+import Link from "next/link";
+import { Card, Image } from "semantic-ui-react";
 
-const ListProducts = ({productList}:{productList:Producto[]}) => (
-    <div>
-      <h1 className={styles.h1}>LISTA DE AVOCADOS</h1>
-      <table className={styles.table}>
-        
-        <thead>
-          <th className={styles.th}>Id</th>
-          <th className={styles.title}>Name</th>
-          <th className={styles.th}>Price</th>
-          <th className={styles.title}>Image</th>
-        </thead>
-        <tbody>
-          {productList?productList.map((prod) => (
-                <tr>
-                  <td className= {styles.textDate}>{prod.id}</td>
-                  <td className= {styles.textDate}>{prod.name}</td>
-                  <td className= {styles.textDate}>{prod.price}</td>
-                  <Link href={`/products/api/${prod.id}`}>
-                    <td className= {styles.textDate}><img className={styles.imag} src={prod.image} alt="Foto"></img> </td>
-                  </Link>
-                </tr>
-              )): null}
-        </tbody>
-      </table>
-    </div>
-    
-  )
+const ListProducts = ({ productList }: { productList: Producto[] }) => (
+  <div>
+    <h1 className={styles.h1}>LISTA DE AVOCADOS</h1>
+    {productList.map((prod) => (
+      <div className="ui container" >
+        <div className="ui three cards"style={{display:"inline-block"}}>
+          <div className="card">
+            <div className="content">
+              <Link href={`/products/api/${prod.id}`}>
+                <Image src={prod.image}></Image>
+                <div className="header">{prod.name}</div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+);
 
-export default ListProducts
+export default ListProducts;
--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-//import styles from '@/styles/footer.module.css';
 
 const Footer = () => {
   return (
