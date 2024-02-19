aaa401985af7c92cad99c6eb8cc02bf5d18059cd
Omar Cabarcas <cahocyjei@gmail.com>
2024-02-19 17:29:57 -0500

    Implement events to nav-bar List to users

--- src/components/navBar/navbar.tsx
+++ src/components/navBar/navbar.tsx
@@ -2,51 +2,34 @@ import React from "react";
 import Link from "next/link";
 import styles from "@styles/nav.module.css";
 import { Menu, Image, Dropdown } from "semantic-ui-react";
-import { idText } from "typescript";
 
-
-
-const NavBar = () => {
-  const options = [
-    { key: 'profile', text: 'Perfil', value: 'profile' },
-    { key: 'loadusers', text: 'Usuarios', value: 'loadusers' },
-    { key: 'registro', text: 'Registro', value: 'registro' },
-    { key: 'logOut', text: 'Log-Out', value: 'login' },
-    { key: 'logIn', text: 'Log-In', value: 'login' },
-
-  ];
-
+export const NavBar = () => {
   return (
-    <div>
+    <div style={{
+      display: "block"
+    }}>
       <Menu className={styles.nav} style={{ background: "red", padding: "20px" }}>
         <Menu.Item as="a">
           <Image className={styles.img} src="/images/pizz.jpg" alt="logo" />
         </Menu.Item>
+        <div style={{
+          fontSize:"30px",
+          marginTop:"70px",
+        }}>
+          <Link href={"/"}>
+            <i className="home icon" style={{color:"white"}}>Home</i>
+          </Link>
+        </div>
         <Menu.Item>
           <h1 className={styles.title} >JeyK</h1>
         </Menu.Item>
-        <Menu.Menu position="right">
-          <Dropdown
-            item
-            text="Opciones"
-          >
-            <Dropdown.Menu>
-              {options.map(option => (
-                <Dropdown.Item 
-                key={option.key}
-                text={option.text}
-                value={option.value}
-                //onClick={(event: React.MouseEvent) => handleOptionClick(event, option.value)}
-                 />
-              ))}
-            </Dropdown.Menu>
-          </Dropdown>
-        </Menu.Menu>
+        <Menu.Item>
+
+        </Menu.Item>
       </Menu>
+
       <div style={{ marginLeft: '50px', fontSize: '30px' }}>
-        <Link href={"/"}>
-          <i className="home icon">Home</i>
-        </Link>
+
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,54 @@
-import { UserFindAllDocument, UserFindAllQuery, UserType } from '@service/graphql-ts/graphql';
-import client from '@service/client';
+import { UserFindAllDocument, UserType } from '@service/graphql-ts/graphql';
 import ListUsers from '@components/users/listUsers';
+import { useQuery } from '@apollo/client';
+import { Menu, Dropdown } from "semantic-ui-react";
+import { useState } from 'react';
 
-let listUsers: UserType[];
-(async () => {
-  const { data } = await client.query({
-    query: (UserFindAllDocument)
-  });
-  if (data === null) throw new Error('Faild to request')
-  listUsers = data.userFindAll as UserType[]
-})();
-
+const options = [
+  { key: 'profile', text: 'Perfil', value: 'profile' },
+  { key: 'loadusers', text: 'Usuarios', value: 'loadusers' },
+  { key: 'registro', text: 'Registro', value: 'registro' },
+  { key: 'logOut', text: 'Log-Out', value: 'logOut' },
+  { key: 'logIn', text: 'Log-In', value: 'logIn' },
+  { key: 'salir', text: 'Salir', value: 'salir' }
+];
 
 const HomePage = () => {
-  console.log(listUsers);
+  const [ isEnabled, setEnabled ] = useState(false);
+  const handleOptionClick = (event: React.MouseEvent, value: string) => {
+    value == 'loadusers' && setEnabled(true);
+    value == 'salir' && setEnabled(false);
+  }
+  const { loading, error, data } = useQuery(UserFindAllDocument);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
   return (
     <div>
-      {listUsers && <ListUsers listUsers={listUsers} />}
+      <div style={{
+      marginLeft:'20px',
+      marginRight:'20px'
+    }}>
+      <Menu position="right" >
+        <Dropdown
+          item
+          text="Opciones"
+        >
+          <Dropdown.Menu>
+            {options.map(option => (
+              <Dropdown.Item
+                key={option.key}
+                text={option.text}
+                value={option.value}
+                onClick={(event: React.MouseEvent) => handleOptionClick(event, option.value)}
+              />
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu>
+      </div>
+      <div>
+        {isEnabled && <ListUsers listUsers={data?.userFindAll as UserType[]} />}
+      </div>
     </div>
   )
 }
